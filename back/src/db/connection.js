da9faf6cbcd0591e996a4e5f7336181085329494
julien-alaria <julienalaria@gmail.com>
2026-02-05 14:29:21 +0100

    implements_awardRouter

--- back/src/db/connection.js
+++ back/src/db/connection.js
@@ -21,9 +21,9 @@ dotenv.config();
  * - Dialect: mysql (type de base de données)
  */
 const sequelize = new Sequelize(
-  process.env.DB_NAME || "marsai_db",
-  process.env.DB_USER || "marsai",
-  process.env.DB_PASSWORD || "Mars2026!",
+  process.env.DB_NAME || "marsai_migrate",
+  process.env.DB_USER || "root",
+  process.env.DB_PASSWORD || "1234",
   {
     host: process.env.DB_HOST || "127.0.0.1",
     port: Number(process.env.DB_PORT) || 3306,
