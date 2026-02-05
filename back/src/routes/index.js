6f5c63c7baaaba73c295d868f330d4329f084ffb
julien-alaria <julienalaria@gmail.com>
2026-02-05 14:29:21 +0100

    implements_awardRouter

--- back/src/routes/index.js
+++ back/src/routes/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import userRouter from "./User.route.js";
 import movieRouter from "./Movie.route.js";
 import authRouter from "./Auth.route.js";
+import awardRouter from "./Award.route.js";
 
 const router = express.Router();
 
@@ -12,5 +13,6 @@ const router = express.Router();
 router.use("/auth", authRouter);      // Routes d'authentification
 router.use("/users", userRouter);     // Routes de gestion utilisateurs
 router.use("/movies", movieRouter);   // Routes de gestion films/vidéos
+router.use("/awards", awardRouter);  // Routes de gestion awards(a corriger, le id du film est géré dans body au lieu de param)
 
 export default router;
