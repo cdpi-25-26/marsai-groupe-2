c6316efe5c448e5d0e956ff945985c40e126fc4c
Celinedev1201 <celine.devaux@laplateforme.io>
2026-02-05 16:34:17 +0100

    feat: ajouter le composant Goals à la page d'accueil

---
+++ front/src/components/home/Goals.jsx
@@ -0,0 +1,9 @@
+export default function Goals() {
+  return (
+    <div className="pt-12 pb-12 w-full">
+      <h2 className="text-4xl font-bold text-center text-[#C27AFF] uppercase">
+        Nos objectifs
+      </h2>
+        </div>
+  );
+}
