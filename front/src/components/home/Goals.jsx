c93e4644b3efe9feb2312b49f7fe682c7e1077a6
Céline Devaux <celine.devaux@laplateforme.io>
2026-02-06 11:39:28 +0100

    feat: Mettre à jour la configuration de la base de données et ajouter le composant InfoCard à la page d'accueil

--- front/src/components/home/Goals.jsx
+++ front/src/components/home/Goals.jsx
@@ -1,9 +1,42 @@
+import InfoCard from "@/components/ui/InfoCard";
+
 export default function Goals() {
   return (
     <div className="pt-12 pb-12 w-full">
-      <h2 className="text-4xl font-bold text-center text-[#C27AFF] uppercase">
-        Nos objectifs
+
+      <h2 className="text-6xl font-bold text-center text-white uppercase">
+        Objectifs du <span className="text-[#F6339A]">festival</span>
       </h2>
-        </div>
+
+      <div className="pt-12 pb-12 pl-12 pr-12 w-full
+                      grid grid-cols-1 md:grid-cols-3
+                      place-items-center gap-10">
+
+        <InfoCard
+          title="GRATUITÉ"
+          description="Conférences et workshops accessibles."
+          accentColor="#00D492"
+          borderColor="border-[rgba(0,212,146,0.40)]"
+          hoverShadow="shadow-[0_0_40px_rgba(0,212,146,0.7)]"
+        />
+
+        <InfoCard
+          title="GRATUITÉ"
+          description="Conférences et workshops accessibles."
+          accentColor="#00D492"
+          borderColor="border-[rgba(0,212,146,0.40)]"
+          hoverShadow="shadow-[0_0_40px_rgba(0,212,146,0.7)]"
+        />
+
+        <InfoCard
+          title="1 MINUTE"
+          description="Format ultra-court pour un impact maximum"
+          accentColor="#C27AFF"
+          borderColor="border-[rgba(194,122,255,0.40)]"
+          hoverShadow="shadow-[0_0_40px_rgba(173,70,255,0.7)]"
+        />
+
+      </div>
+    </div>
   );
 }
