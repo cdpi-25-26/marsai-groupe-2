ff0c93f35e12688075740ce7cc2da9ee61f105fc
julien-alaria <julienalaria@gmail.com>
2026-02-06 09:07:36 +0100

    debug_routes

--- front/src/api/config.js
+++ front/src/api/config.js
@@ -6,7 +6,7 @@ import axios from "axios";
  * Timeout: 5000ms (increased for DB operations)
  */
 const instance = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: "http://localhost:3000/",
   timeout: 5000,
 });
 
