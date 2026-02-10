7dc3e4f47582b244f447ff248adde50f02fbdab4
julien-alaria <julienalaria@gmail.com>
2026-02-10 11:26:53 +0100

    merged from chainez branch

--- back/src/routes/index.js
+++ back/src/routes/index.js
@@ -5,7 +5,6 @@ import authRouter from "./Auth.route.js";
 import awardRouter from "./Award.route.js";
 import voteRouter from "./Vote.route.js";
 import categorieRouter from "./Categorie.route.js";
-//import collaboratorRouter from "./Collaborator.route.js";
 import reservationRouter from "./Reservation.route.js";import eventRouter from "./Event.route.js";
 
 const router = express.Router();
@@ -15,13 +14,19 @@ const router = express.Router();
  * Chaque groupe de routes est préfixé par son chemin
  */
 router.use("/auth", authRouter);      // Routes d'authentification
+
 router.use("/users", userRouter);     // Routes de gestion utilisateurs
+
 router.use("/movies", movieRouter);   // Routes de gestion films/vidéos
+
 router.use("/awards", awardRouter);  // Routes de gestion awards
+
 router.use("/votes", voteRouter); // Routes de gestion votes
+
 router.use("/categories", categorieRouter); // Routes de gestion catégories
-//router.use("/collaborators", collaboratorRouter); // Routes de gestion collaborateurs
+
 router.use("/reservations", reservationRouter); // Routes de gestion réservations
+
 router.use("/events", eventRouter); // Routes de gestion réservations
 
 export default router;
