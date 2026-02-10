6b06c5869c564776f77d2a46b73837b764664a0b
Marco Antonio PINNA <pinna.marcantonio@gmail.com>
2026-02-10 16:13:12 +0100

    feat: enhance TitleInBox component and update Movie routes for better organization

--- front/src/components/TitleInBox.jsx
+++ front/src/components/TitleInBox.jsx
@@ -5,6 +5,7 @@ export default function TitleInBox({
     spancolor,
     title2,
 }) {
+    const spanStyle = spancolor ? { color: spancolor } : undefined;
     return (
          <div className="inline-flex items-center justify-center 
                           bg-black/40 border border-white/40 
@@ -12,9 +13,9 @@ export default function TitleInBox({
                      <span style={{ color: iconcolor }} className="pr-2 flex items-center">
   {icon}
 </span>
-            <p className="tracking-[0.25em] text-gray-300 font-bold uppercase m-0">
-              {title} <span className={`text-[${spancolor}]`}>{title2}</span>
-            </p>
+            <div className="tracking-[0.25em] text-gray-300 font-bold uppercase">
+              {title} {title2 ? <span style={spanStyle}>{title2}</span> : null}
+            </div>
           </div>
     )
 }
