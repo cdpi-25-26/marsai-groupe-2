4ff8a1a45c476b20b6eae8e3568ad044bc4fd2f3
Marco Antonio PINNA <pinna.marcantonio@gmail.com>
2026-02-18 15:54:45 +0100

    feat(db): remove default values for database connection parameters

--- back/src/db/connection.js
+++ back/src/db/connection.js
@@ -21,12 +21,12 @@ dotenv.config();
  * - Dialect: mysql (type de base de données)
  */
 const sequelize = new Sequelize(
-  process.env.DB_NAME || "marsai",
-  process.env.DB_USER || "root",
-  process.env.DB_PASSWORD || "josantos",
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
   {
-    host: process.env.DB_HOST || "127.0.0.1",
-    port: Number(process.env.DB_PORT) || 3306,
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
     dialect: "mysql",
   }
 );
