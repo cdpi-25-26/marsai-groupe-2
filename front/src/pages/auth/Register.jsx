60b91e1ede3a0942f7a30e6cb71cc2d590788626
chainez-hat <yanina.ens@gmail.com>
2026-02-25 14:24:49 +0100

    mettre a jour les fichier de langues et rejister.jsx pour le titre et le sous-titre de la page

--- front/src/pages/auth/Register.jsx
+++ front/src/pages/auth/Register.jsx
@@ -137,9 +137,9 @@ export function Register() {
       <div className="min-h-screen bg-black text-white font-light pt-32 pb-6 px-3 md:pt-36">
         <div className="max-w-7xl mx-auto">
           <div className="text-center mb-4">
-            <h1 className="text-3xl font-bold mt-1">Créer mon profil Producteur</h1>
+            <h1 className="text-3xl font-bold mt-1">{t('forms.register.title')}</h1>
             <p className="text-gray-400 text-xs mt-1">
-              Complétez votre profil producteur. Vous pourrez ensuite déposer votre film dans votre espace producteur.
+              {t('forms.register.subtitle1')}
             </p>
           </div>
 
