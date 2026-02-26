cae074627e54597b4881104fdbf9d50854129110
chainez-hat <yanina.ens@gmail.com>
2026-02-26 10:36:52 +0100

    mettre à jour les fichiers de langues et connecter les fivhiers Info.jsx et Sponsors.jsx au systeme de traduction

--- front/src/pages/public/Sponsors.jsx
+++ front/src/pages/public/Sponsors.jsx
@@ -1,32 +1,34 @@
 import data from "../../assets/data/categoriesPartenaires.json";
 import PartenaireGrid from "../../components/PartenairesGrid";
+import { useTranslation } from "react-i18next";
 
 export default function Partenaires() {
+  const { t } = useTranslation();
   return (
     <>
       <div className="container mx-auto px-6 py-12">
 
         {/* TITRE OFFICIELS */}
         <h2 className="text-4xl md:text-6xl font-bold text-white uppercase text-center pb-6">
-          PARTENAIRES <span className="text-[#F6339A]">OFFICIELS</span>
+          {t("pages.sponsors.title")}{" "} <span className="text-[#F6339A]">{t("pages.sponsors.categories.officiels")}</span>
         </h2>
         <PartenaireGrid items={data.officiels} />
 
         {/* TITRE MEDIAS */}
         <h2 className="text-4xl md:text-6xl font-bold text-white uppercase text-center pb-6">
-          PARTENAIRES <span className="text-[#F6339A]">Médias</span>
+          {t("pages.sponsors.title")}{" "} <span className="text-[#F6339A]">{t("pages.sponsors.categories.medias")}</span>
         </h2>
         <PartenaireGrid items={data.medias} />
 
         {/* TITRE TECHNIQUES */}
         <h2 className="text-4xl md:text-6xl font-bold text-white uppercase text-center pb-6">
-          PARTENAIRES <span className="text-[#F6339A]">Techniques</span>
+          {t("pages.sponsors.title")}{" "} <span className="text-[#F6339A]">{t("pages.sponsors.categories.techniques")}</span>
         </h2>
         <PartenaireGrid items={data.techniques} />
 
         {/* TITRE DIVERS */}
         <h2 className="text-4xl md:text-6xl font-bold text-white uppercase text-center pb-6">
-          PARTENAIRES <span className="text-[#F6339A]">Divers</span>
+          {t("pages.sponsors.title")}{" "} <span className="text-[#F6339A]">{t("pages.sponsors.categories.divers")}</span>
         </h2>
         <PartenaireGrid items={data.divers} />
 
