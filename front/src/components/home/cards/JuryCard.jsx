95930b1724e796f83bf3a523a79f9336d112c1ad
Celinedev1201 <celine.devaux@laplateforme.io>
2026-03-03 12:47:09 +0100

    Refactor JuryCard component: update styling for improved visual appeal and responsiveness

--- front/src/components/home/cards/JuryCard.jsx
+++ front/src/components/home/cards/JuryCard.jsx
@@ -1,7 +1,7 @@
 export default function JuryCard({ image, membertitle, membersstitle }) {
   return (
     <div
-      className="relative rounded-xl w-64 h-64 sm:w-72 sm:h-72 md:w-80 md:h-80 lg:w-80 lg:h-80 overflow-hidden shadow-[0_0_10px_rgba(0,0,0,0.2)] transition-shadow duration-200"
+      className="relative rounded-xl w-64 h-64 sm:w-72 sm:h-72 md:w-80 md:h-80 lg:w-80 lg:h-80 overflow-hidden border-2 shadow-lg transition-transform duration-300 hover:scale-105 cursor-pointer"
       style={{
         backgroundImage: `url(${image})`,
         backgroundSize: 'cover',
@@ -9,7 +9,7 @@ export default function JuryCard({ image, membertitle, membersstitle }) {
       }}
     >
       {/* Overlay permanent à 50% */}
-      <div className="absolute inset-0 flex flex-col justify-center items-center text-center hover:bg-[rgba(0,0,0,0.8)] px-2 z-20">
+      <div className="absolute inset-0 flex flex-col justify-center items-center text-center hover:bg-[rgba(0,0,0,0.8)] px-2 z-20 ">
       <div className="justify-center items-center text-center z-30 absolute inset-0 flex flex-col opacity-0 hover:opacity-100">
          <h2 className="text-[#2B7FFF] font-bold text-2xl">{membertitle}</h2>
         <p className="text-white text-xl mt-1">{membersstitle}</p>
