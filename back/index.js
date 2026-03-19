296ea8b1b71414b655e12d8d22c940f7748a2b9d
MelodieGRAMLICH <melodie.gramlich@laplateforme.io>
2026-03-19 15:31:51 +0100

    Mise à jour de la configuration CORS pour autoriser toutes les origines et ajout des en-têtes nécessaires dans l'instance Axios

--- back/index.js
+++ back/index.js
@@ -21,14 +21,19 @@ const PORT = process.env.PORT || 3000;
 /**
  * Configuration des middlewares
  */
-app.use(cors({
+/**app.use(cors({
   origin: process.env.FRONTEND_URL || "http://localhost:5173",
   credentials: true
 }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/uploads", express.static(path.join(process.cwd(), "uploads")));
-
+**/
+app.use(cors({ origin: "*" ,
+   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ['Content-Type', 'Authorization', 'ngrok-skip-browser-warning'],
+})); // Autoriser les requêtes CORS de toutes origines
+app.use(express.json());
 /**
  * Routes de l'API
  */
